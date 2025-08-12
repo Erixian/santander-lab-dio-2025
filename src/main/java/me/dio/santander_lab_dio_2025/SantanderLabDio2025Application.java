d15517c6321066f7aa84d19c5bf0b72e81f31759
erixian <cibercianocode@gmail.com>
2025-08-12 20:34:34 -0300

    Initial commit

---
+++ src/main/java/me/dio/santander_lab_dio_2025/SantanderLabDio2025Application.java
@@ -0,0 +1,13 @@
+package me.dio.santander_lab_dio_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderLabDio2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderLabDio2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/santander_lab_dio_2025/SantanderLabDio2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.santander_lab_dio_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderLabDio2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
