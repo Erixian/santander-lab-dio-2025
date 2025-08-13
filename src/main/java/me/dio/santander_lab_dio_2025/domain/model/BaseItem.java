f26cff29995ce99e4ffb93675c44b408dfbe6555
$rip <cibercianocode@gmail.com>
2025-08-13 11:29:34 -0300

    Mapeamento das classes finalizado

---
+++ src/main/java/me/dio/santander_lab_dio_2025/domain/model/BaseItem.java
@@ -0,0 +1,39 @@
+package me.dio.santander_lab_dio_2025.domain.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseItem {
+    private Long id;
+    private String icon;
+    private String description;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
